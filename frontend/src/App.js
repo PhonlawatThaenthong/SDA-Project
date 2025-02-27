8f522d1e6a5f5e1bed44c8f6081568fd6ca158d0
Pongsaton SaeFung <aunggrit.sae@gmail.com>
2025-02-27 22:12:37 +0700

    commit before merge

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -68,11 +68,6 @@ const App = () => {
       <Space direction="vertical" style={{ width: '100%' }}>
         <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
           <Text strong>พื้นที่เก็บข้อมูล</Text>
-          <Tooltip title="คลิกเพื่อซื้อพื้นที่เพิ่มเติม">
-            <Button type="link" size="small" icon={<PlusOutlined />} style={{ padding: 0 }}>
-              เพิ่มพื้นที่
-            </Button>
-          </Tooltip>
         </div>
         
         <Progress percent={storageUsed} size="small" strokeColor={{
